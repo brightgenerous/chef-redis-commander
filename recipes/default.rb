16ff6555711003783c361fc151b54dfbedb44fed
root <root@innovotech-n1.(none)>
2014-10-09 21:37:04 +0800

    add supervisord to monitor redis-commander

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,21 @@
+
+include_recipe "node"
+
 # Node module Redis commander install
 node_npm "redis-commander" do
-    action :install
+  action :install
+end
+
+
+include_recipe "supervisor"
+
+supervisor_service "redis-commander" do
+  command "redis-commander"
+  stdout_logfile "syslog"
+  stderr_logfile "syslog"
+  startsecs 10
+  stopsignal "QUIT"
+  stopasgroup true
+  killasgroup true
+  process_name '%(program_name)s'
 end
